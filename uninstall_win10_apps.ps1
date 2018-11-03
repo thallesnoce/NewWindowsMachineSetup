30c7c0c8504bcda0e5e7440400e059448d098658
unknown <thallesnoce@gmail.com>
2018-11-03 23:53:20 +0000

    First draft of scripts (chocolatey and powershell) to automate configuration of my personal machine.

---
+++ uninstall_win10_apps.ps1
@@ -0,0 +1,16 @@
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *people* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+Get-AppxPackage *A278AB0D.MarchofEmpires* | Remove-AppxPackage
+Get-AppxPackage *CandyCrushSodaSaga* | Remove-AppxPackage
+Get-AppxPackage *CandyCrushSaga* | Remove-AppxPackage
+Get-AppxPackage *HiddenCityMysteryofShadows* | Remove-AppxPackage
