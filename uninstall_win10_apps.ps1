9d1b376a68133e32efa40b0bfa91d6e594c74abd
unknown <thallesnoce@gmail.com>
2019-09-23 22:28:42 -0300

    Included new commands

--- uninstall_win10_apps.ps1
+++ uninstall_win10_apps.ps1
@@ -1,16 +1,31 @@
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
+#run as admin
+#Get-AppxPackage -AllUsers | ft Name, PackageFullName -
+#Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage –online
 
-Get-AppxPackage *bingnews* | Remove-AppxPackage
-Get-AppxPackage *onenote* | Remove-AppxPackage
-Get-AppxPackage *people* | Remove-AppxPackage
-Get-AppxPackage *windowsphone* | Remove-AppxPackage
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
-Get-AppxPackage *A278AB0D.MarchofEmpires* | Remove-AppxPackage
-Get-AppxPackage *CandyCrushSodaSaga* | Remove-AppxPackage
-Get-AppxPackage *CandyCrushSaga* | Remove-AppxPackage
-Get-AppxPackage *HiddenCityMysteryofShadows* | Remove-AppxPackage
+#Removes everything that is possible to be removed
+#Get-AppxPackage -AllUsers | Remove-AppxPackage
+
+#Remove one-by-one
+Get-AppxPackage -allusers *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage -allusers *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage -allusers *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage -allusers *bingfinance* | Remove-AppxPackage
+Get-AppxPackage -allusers *zunevideo* | Remove-AppxPackage
+Get-AppxPackage -allusers *bingnews* | Remove-AppxPackage
+Get-AppxPackage -allusers *onenote* | Remove-AppxPackage
+Get-AppxPackage -allusers *people* | Remove-AppxPackage
+Get-AppxPackage -allusers *windowsphone* | Remove-AppxPackage
+Get-AppxPackage -allusers *bingsports* | Remove-AppxPackage
+Get-AppxPackage -allusers *xboxapp* | Remove-AppxPackage
+Get-AppxPackage -allusers *A278AB0D.MarchofEmpires* | Remove-AppxPackage
+Get-AppxPackage -allusers *CandyCrushSodaSaga* | Remove-AppxPackage
+Get-AppxPackage -allusers *CandyCrushSaga* | Remove-AppxPackage
+Get-AppxPackage -allusers *HiddenCityMysteryofShadows* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.BingWeather* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.Messaging* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.MixedReality.Portal* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.Wallet* | Remove-AppxPackage
+Get-AppxPackage -allusers *Microsoft.SkypeApp* | Remove-AppxPackage
+Get-AppxPackage -allusers *king.com.CandyCrushSaga* | Remove-AppxPackage
+Get-AppxPackage -allusers *king.com.CandyCrushFriends* | Remove-AppxPackage
